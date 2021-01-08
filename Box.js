f89f884494fb0dbc5917b4f3025733ab94d13933
Ananyaa2007 <75204641+Ananyaa2007@users.noreply.github.com>
2021-01-08 17:55:55 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,9 +1,7 @@
 class Box{
     constructor(x, y, width, height) {
         var options = {
-            'restitution':0.8,
-            'friction':1.0,
-            'density':1.0,
+            'restitution':1,
             // isStatic :true
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
